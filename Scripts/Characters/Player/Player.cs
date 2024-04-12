ce51ac1d22e83dbbe43641c8d0513aaab7b5e85c
Yaphet Abraha <yaphet.abraha@gmail.com>
2024-04-12 09:57:03 -0500

    Initial commit

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer animPlayerNode;
+    [Export] private Sprite3D sprite3DNode;
+    private Vector2 direction = new();
+
+    public override void _Ready()
+    {
+        animPlayerNode.Play(GameConstants.ANIM_IDLE);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = new(direction.X, 0, direction.Y);
+        Velocity *= 5;
+
+        MoveAndSlide();
+
+        Flip();
+
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        //arguments are located in the actions section in Godot [Project Settings]
+        direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_BACKWARD, GameConstants.INPUT_MOVE_FORWARD);
+
+        if (direction == Vector2.Zero)
+        {
+            animPlayerNode.Play(GameConstants.ANIM_IDLE);
+        }
+        else
+        {
+            animPlayerNode.Play(GameConstants.ANIM_MOVE);
+        }
+
+
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0;
+
+        if (isNotMovingHorizontally)
+        {
+            return;
+        }
+
+        bool isMovingLeft = Velocity.X < 0;
+        sprite3DNode.FlipH = isMovingLeft;
+
+    }
+}
